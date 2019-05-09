714dbefcc4e41b21ba2de191a746cf08e9e3200c
Yosuke <s7k315@gmail.com>
2019-05-09 14:51:51 +0900

    remove console.log

--- app/assets/javascripts/users_index.js
+++ app/assets/javascripts/users_index.js
@@ -29,7 +29,6 @@ $(function() {
 
     $('#user-search-field').on('keyup', function() {
       var input = $('#user-search-field').val();
-      console.log(input);
       $.ajax({
         type: 'GET',
         url: '/users',
